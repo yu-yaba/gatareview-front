0a33c1c359e9a07326711057c0f5cb510af9922f
yu-yaba <kawai.yuya3401@gmail.com>
2023-09-15 16:20:22 +0900

    loadingの追加

--- app/lectures/page.tsx
+++ app/lectures/page.tsx
@@ -5,6 +5,7 @@ import { LectureSchema } from '../types/LectureSchema';
 import Link from "next/link";
 import { handleAjaxError } from '../helpers/helpers';
 import { useRouter } from 'next/navigation';
+import Loading from 'react-loading';
 
 const LectureList = () => {
   const [searchWord, setSearchWord] = useState('');
@@ -14,11 +15,13 @@ const LectureList = () => {
   const [fetchedLectures, setFetchedLectures] = useState<Array<LectureSchema>>([]);
   const [searchButtonClicked, setSearchButtonClicked] = useState(false);
   const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (searchWord || selectedFaculty) {
       const fetchData = async () => {
         try {
+          setIsLoading(true);
           const query = [
             searchWord && `searchWord=${searchWord}`,
             selectedFaculty && `faculty=${selectedFaculty}`,
@@ -32,6 +35,7 @@ const LectureList = () => {
         } catch (error) {
           handleAjaxError("検索条件を入力してください");
         }
+        setIsLoading(false);
       };
 
       fetchData();
@@ -48,6 +52,7 @@ const LectureList = () => {
     if (initialSearchWord || initialSelectedFaculty || initialSortType) {
       const fetchData = async () => {
         try {
+          setIsLoading(true);
           const query = [
             initialSearchWord && `searchWord=${initialSearchWord}`,
             initialSelectedFaculty && `faculty=${initialSelectedFaculty}`,
@@ -60,6 +65,7 @@ const LectureList = () => {
         } catch (error) {
           handleAjaxError("検索条件を入力してください");
         }
+        setIsLoading(false);
       };
 
       fetchData();
@@ -147,77 +153,85 @@ const LectureList = () => {
 
   return (
     <section className="text-center">
-      <div className="flex justify-center ">
-        <div className="flex flex-wrap justify-center items-center w-10/12 2xl:w-7/12 border border-1 py-6 px-3 rounded-2xl shadow-inner bg-slate-50 text-sm md:text-lg font-bold">
-          <div className='flex justify-center w-full md:w-7/12 items-center mb-4 md:mb-0'>
-            <label className='border-4 w-9/12 text-start pl-4 border-green-400 rounded-lg inline-block py-3 bg-white hover:bg-green-50 text-gray-600'>
-              キーワード
-              <input
-                className="ml-2 w-1/2 mr-0 outline-none  hover:bg-green-50"
-                placeholder="授業・教授・学部"
-                type="text"
-                ref={searchInput}
-                value={searchWord}
-                onChange={updateSearchWord}
-              />
-            </label>
-            <button
-              onClick={() => {
-                const query = [
-                  searchWord && `searchWord=${searchWord}`,
-                  selectedFaculty && `faculty=${selectedFaculty}`,
-                  sortType && `sortType=${sortType}`
-                ].filter(Boolean).join('&');
-                router.push(`/lectures?${query}`);
-                setSearchButtonClicked(true);
-              }}
-              className=' bg-green-500 text-white w-2/12 md:w-2/12 text-sm md:text-lg rounded-md py-3 border border-2 ml-2 border-green-500'
-            >
-              検索
-            </button>
-          </div>
-          <div className=' w-full md:w-5/12'>
-            <select id="faculty" name="faculty"
-              onChange={handleSelectChange(setSelectedFaculty)} value={selectedFaculty}
-              className=" p-3 px-2 md:ml-4 w-6/12 md:w-5/12 border-4 rounded-lg text-gray-600 border-green-400 outline-none hover:bg-green-50">
-              <option value="">学部で検索</option>
-              <option value="G: 教養科目">G: 教養科目</option>
-              <option value="H: 人文学部">H: 人文学部</option>
-              <option value="K: 教育学部">K: 教育学部</option>
-              <option value="L: 法学部">L: 法学部</option>
-              <option value="E: 経済科学部">E: 経済科学部</option>
-              <option value="S: 理学部">S: 理学部</option>
-              <option value="M: 医学部">M: 医学部</option>
-              <option value="D: 歯学部">D: 歯学部</option>
-              <option value="T: 工学部">T: 工学部</option>
-              <option value="A: 農学部">A: 農学部</option>
-              <option value="X: 創生学部">X: 創生学部</option>
-            </select>
-            <select
-              id="sortType"
-              name="sortType"
-              onChange={handleSelectChange(setSortType)}
-              value={sortType}
-              className="py-3 px-2 w-5/12 ml-2 md:ml-4 border-4 rounded-lg text-gray-600 border-green-400 outline-none hover:bg-green-50"
-            >
-              <option value="newest">新しい順</option>
-              <option value="highestRating">評価が高い順</option>
-              <option value="mostReviewed">レビュー件数順</option>
-            </select>
+      {isLoading ? (
+        <div className="flex justify-center items-center h-screen">
+          <Loading type={"bubbles"} color={"#1DBE67"} />
+        </div>
+      ) : (
+        <>
+          <div className="flex justify-center ">
+            <div className="flex flex-wrap justify-center items-center w-10/12 2xl:w-7/12 border border-1 py-6 px-3 rounded-2xl shadow-inner bg-slate-50 text-sm md:text-lg font-bold">
+              <div className='flex justify-center w-full md:w-7/12 items-center mb-4 md:mb-0'>
+                <label className='border-4 w-9/12 text-start pl-4 border-green-400 rounded-lg inline-block py-3 bg-white hover:bg-green-50 text-gray-600'>
+                  キーワード
+                  <input
+                    className="ml-2 w-1/2 mr-0 outline-none  hover:bg-green-50"
+                    placeholder="授業・教授・学部"
+                    type="text"
+                    ref={searchInput}
+                    value={searchWord}
+                    onChange={updateSearchWord}
+                  />
+                </label>
+                <button
+                  onClick={() => {
+                    const query = [
+                      searchWord && `searchWord=${searchWord}`,
+                      selectedFaculty && `faculty=${selectedFaculty}`,
+                      sortType && `sortType=${sortType}`
+                    ].filter(Boolean).join('&');
+                    router.push(`/lectures?${query}`);
+                    setSearchButtonClicked(true);
+                  }}
+                  className=' bg-green-500 text-white w-2/12 md:w-2/12 text-sm md:text-lg rounded-md py-3 border border-2 ml-2 border-green-500'
+                >
+                  検索
+                </button>
+              </div>
+              <div className=' w-full md:w-5/12'>
+                <select id="faculty" name="faculty"
+                  onChange={handleSelectChange(setSelectedFaculty)} value={selectedFaculty}
+                  className=" p-3 px-2 md:ml-4 w-6/12 md:w-5/12 border-4 rounded-lg text-gray-600 border-green-400 outline-none hover:bg-green-50">
+                  <option value="">学部で検索</option>
+                  <option value="G: 教養科目">G: 教養科目</option>
+                  <option value="H: 人文学部">H: 人文学部</option>
+                  <option value="K: 教育学部">K: 教育学部</option>
+                  <option value="L: 法学部">L: 法学部</option>
+                  <option value="E: 経済科学部">E: 経済科学部</option>
+                  <option value="S: 理学部">S: 理学部</option>
+                  <option value="M: 医学部">M: 医学部</option>
+                  <option value="D: 歯学部">D: 歯学部</option>
+                  <option value="T: 工学部">T: 工学部</option>
+                  <option value="A: 農学部">A: 農学部</option>
+                  <option value="X: 創生学部">X: 創生学部</option>
+                </select>
+                <select
+                  id="sortType"
+                  name="sortType"
+                  onChange={handleSelectChange(setSortType)}
+                  value={sortType}
+                  className="py-3 px-2 w-5/12 ml-2 md:ml-4 border-4 rounded-lg text-gray-600 border-green-400 outline-none hover:bg-green-50"
+                >
+                  <option value="newest">新しい順</option>
+                  <option value="highestRating">評価が高い順</option>
+                  <option value="mostReviewed">レビュー件数順</option>
+                </select>
 
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div>
-        <div className=' flex justify-center mt-6'>
-          <div className='flex justify-end w-8/12 text-sm md:text-base 2xl:w-7/12'>
-            <p><span className=' font-bold text-green-600'>{renderLectures().length}</span> 件を表示中</p>
+          <div>
+            <div className=' flex justify-center mt-6'>
+              <div className='flex justify-end w-8/12 text-sm md:text-base 2xl:w-7/12'>
+                <p><span className=' font-bold text-green-600'>{renderLectures().length}</span> 件を表示中</p>
+              </div>
+            </div>
+            <ul className="list-none mt-4 p-0">
+              {renderLectures()}
+            </ul>
           </div>
-        </div>
-        <ul className="list-none mt-4 p-0">
-          {renderLectures()}
-        </ul>
-      </div>
+        </>
+      )}
     </section>
   );
 };
