05dabf4cbaefb8151c8eb5e2e4314c2f6c26973b
yu-yaba <kawai.yuya3401@gmail.com>
2024-02-10 11:02:19 +0900

    ヘッダーのボタンを削除、

--- app/header.tsx
+++ app/header.tsx
@@ -9,13 +9,6 @@ export default function Header() {
             <img src="/white-title.png" alt="title" className="w-[160px] md:w-[40%] md:h-[80%]" />
           </div>
         </Link>
-        <div className="pr-7 md:pr-10">
-          <Link href="/lectures/new" className="inline-flex items-center">
-            <button className="bg-white text-[#1dbe67] font-bold text-sm md:text-lg  w-full p-2 md:p-3 rounded-lg shadow-lg transform hover:scale-110 transition-transform duration-200 ease-in-out">
-              授業を登録
-            </button>
-          </Link>
-        </div>
       </header>
     </>
   )
