fae0899683b7ce9c44b29ec8adae2f55ea7cd77a
yu-yaba <kawai.yuya3401@gmail.com>
2025-05-24 22:04:04 +0900

    fix:auth.tsで認証設定を管理するように修正

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,5 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-}
+import { authOptions } from "@/app/lib/auth"
 
 const handler = NextAuth(authOptions)
 
