5286c11ca16b9c95338e6934747d65ccc51cf34b
yu-yaba <kawai.yuya3401@gmail.com>
2025-05-25 18:23:41 +0900

    Remove Jest-related files and dependencies

--- __mocks__/next-auth/react.js
+++
@@ -1,7 +0,0 @@
-export const useSession = jest.fn();
-export const signIn = jest.fn();
-export const signOut = jest.fn();
-
-// You can add a SessionProvider mock if needed for other tests,
-// but for AuthButtons, mocking useSession is usually sufficient.
-export const SessionProvider = ({ children }) => children;
