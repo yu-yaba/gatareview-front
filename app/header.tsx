3d97c8af69af80073076e1d3d1ff250a73926c21
yu-yaba <kawai.yuya3401@gmail.com>
2025-06-09 00:33:02 +0900

    アイコンのリンクをルートページに変更

--- app/header.tsx
+++ app/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <>
       <header className="bg-[#1DBE67] h-20 md:h-[90px] flex justify-between items-center shadow-md mb-4">
-        <Link href="/lectures" className="flex items-center">
+        <Link href="/" className="flex items-center">
           <div className="pl-7 md:pl-10 flex">
             <img src="/white-title.png" alt="title" className="w-[160px] md:w-[40%] md:h-[80%]" />
           </div>
