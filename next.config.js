4913345dcbbfed4892bbf8a277cc9e4ed1ecfa16
yu-yaba <kawai.yuya3401@gmail.com>
2025-06-22 20:29:41 +0900

    fix🐛:開発環境でのService Worker生成を無効化

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: false,
+  disable: process.env.NODE_ENV === 'development',
   runtimeCaching: [
     {
       urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
